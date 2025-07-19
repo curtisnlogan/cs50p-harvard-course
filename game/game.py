01b8b19aed68ed907355919f05f91f69d9bfa5ea
curtis <curtisnlogan@gmail.com>
2025-07-19 18:21:33 +0100

    pass

--- game/game.py
+++ game/game.py
@@ -1,24 +1,50 @@
+"""
+pseudocode
+"""
+
 import random
 
+
+# all code to be executed is contained within this single function
 def main():
     def guessing_game():
         while True:
+            # error catching, incorrect user input is a common one
             try:
-                user_n = int(input("Level: "))
-            except (TypeError, ValueError):
-            random_n = random.randint(1, user_n)
-            break
+                user_level = int(input("Level: "))
+            # catching input that cannot be converted to an integer
+            except ValueError:
+                # force loop restart otherwise program will continue
+                # and the user_level variable will throw error as it won't
+                # exist!
+                continue
+            if user_level < 1:
+                # this will re-prompt as pass means do nothing
+                pass
+            # code is now okay so lets break out of this infinite loop here
+            else:
+                break
+            # random.randrange was problematic if the user inputted a level
+            # of 1, as there is of-course no range between 1 and 1
+        random_n = random.randint(1, user_level)
         while True:
             try:
                 user_guess = int(input("Guess: "))
-                if user_guess < random_n:
-                    print("Too small!")
-                elif user_guess > random_n:
-                    print("Too large!")
-                else:
-                    print("Just right!")
-                    break
-            except (TypeError, ValueError):
+            except ValueError:
+                continue
+            if user_guess < 1:
+                # including pass makes it more readable, implies the loop
+                # will restart again
+                pass
+            elif user_guess < random_n:
+                print("Too small!")
+                pass
+            elif user_guess > random_n:
+                print("Too large!")
+                pass
+            else:
+                print("Just right!")
+                break
 
     guessing_game()
 
